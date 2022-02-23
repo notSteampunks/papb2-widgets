d28f119abcf45b579abdddc632e871163a59403e
notSteampunks <isnanda.a@mail.ugm.ac.id>
2022-02-23 10:45:43 +0700

    add widget id

--- app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
@@ -13,10 +13,9 @@ public class NewAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, "APP WIDGET");
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
